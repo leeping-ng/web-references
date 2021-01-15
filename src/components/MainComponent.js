fa6692f33b7cfb994aa2e59092ae7f2c930ed207
leeping-ng <leeping_ng@hotmail.com>
2021-01-15 15:14:24 +0800

    header and footer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
 
 class Main extends Component {
@@ -21,18 +22,15 @@ class Main extends Component {
   	render() {
     	return (
       		<div>
-        		<Navbar dark color="primary">
-          			<div className="container">
-            			<NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          			</div>
-        		</Navbar>
+				<Header />
 				<Menu dishes={this.state.dishes}
 					// Takes in dish.id from clicked card in MenuComponent.js and calls onDishSelect function 
             		onClick={(dishId) => this.onDishSelect(dishId)}/>
 				<DishDetail 
 					// "Filter" creates an array filled with all array elements (dish) that pass a test
             		dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-      		</div>
+				<Footer />
+			</div>
     	);
   	}
 }
