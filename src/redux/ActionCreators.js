5fc5f5d3b82c689f05be96c119e3cedfe42dfe5c
leeping-ng <leeping_ng@hotmail.com>
2021-01-24 19:20:23 +0800

    assignment 4

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,6 +1,7 @@
 import * as ActionTypes from './ActionTypes';
 import { baseUrl } from '../shared/baseUrl';
 
+// Note that this is different from addComments defined later. This is literally for adding new comments to the page.
 export const addComment = (comment) => ({
     type: ActionTypes.ADD_COMMENT,
     payload: comment
@@ -43,9 +44,56 @@ export const postComment = (dishId, rating, author, comment) => (dispatch) => {
     .then(response => response.json())
     .then(response => dispatch(addComment(response)))
     .catch(error => { console.log('Post comments', error.message)
-        alert('Your comment could not be posted\nError: '+ error.message)})
+        alert('Your comments could not be posted\nError: '+ error.message)})
 
 }
+
+
+export const postFeedback = (firstname, lastname, telnum, email, agree, contactType, message) => (dispatch) => {
+
+    const newFeedback = {
+        firstname: firstname,
+        lastname: lastname,
+        telnum: telnum,
+        email: email,
+        agree: agree,
+        contactType: contactType,
+        message: message
+    }
+
+    return fetch(baseUrl + 'feedback', {
+        method: 'POST',
+        body: JSON.stringify(newFeedback),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        credentials: 'same-origin'
+    })
+    // error from server
+    .then(response => {
+        if (response.ok) {
+            return response;
+        }
+        else {
+            var error = new Error('Error ' + response.status + ': ' + response.statusText);
+            error.response = response;
+            throw error;
+        }
+    },
+    // Don't hear back from server
+    error => {
+        var errmess = new Error(error.message);
+        throw errmess;
+    })
+    .then(response => response.json())
+    .then(response =>
+        alert("Thank you for your feedback!" + JSON.stringify(response))
+      )    
+    .catch(error => { console.log('Post feedback', error.message)
+        alert('Your feedback could not be posted\nError: '+ error.message)})
+
+}
+
 
 export const fetchDishes = () => (dispatch) => {
     dispatch(dishesLoading(true));
@@ -86,6 +134,8 @@ export const addDishes = (dishes) => ({
     payload: dishes
 });
 
+
+
 export const fetchComments = () => (dispatch) => {
     return fetch(baseUrl + 'comments')
         // error from server
@@ -114,10 +164,13 @@ export const commentsFailed = (errmess) => ({
     payload: errmess
 });
 
+// Note that this is different from addComment defined earlier. This is for fetch.
 export const addComments = (comments) => ({
     type: ActionTypes.ADD_COMMENTS,
     payload: comments
 });
+
+
 
 export const fetchPromos = () => (dispatch) => {
     dispatch(promosLoading(true));
@@ -156,4 +209,45 @@ export const promosFailed = (errmess) => ({
 export const addPromos = (promos) => ({
     type: ActionTypes.ADD_PROMOS,
     payload: promos
+});
+
+
+
+export const fetchLeaders = () => (dispatch) => {
+    dispatch(leadersLoading(true));
+
+    return fetch(baseUrl + 'leaders')
+        // error from server
+        .then(response => {
+            if (response.ok) {
+                return response;
+            }
+            else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        },
+        // Don't hear back from server
+        error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+        })
+        .then(response => response.json())
+        .then(leaders => dispatch(addLeaders(leaders)))
+        .catch(error => dispatch(leadersFailed(error.message)));
+}
+
+export const leadersLoading = () => ({
+    type: ActionTypes.LEADERS_LOADING
+});
+
+export const leadersFailed = (errmess) => ({
+    type: ActionTypes.LEADERS_FAILED,
+    payload: errmess
+});
+
+export const addLeaders = (leaders) => ({
+    type: ActionTypes.ADD_LEADERS,
+    payload: leaders
 });
