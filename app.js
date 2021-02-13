d5a0a138c434a1d436e883cd3a62cd49353a3650
leeping-ng <leeping_ng@hotmail.com>
2021-02-13 09:46:22 +0800

    express rest api with mongoose part 1

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
+// a lot of boilerplate code below created from express-generator
+
 var createError = require('http-errors');
-var express = require('express');
+var express = require('express'); // express Router to break up express and tidy code
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var logger = require('morgan'); // morgan for logging information
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+
+const mongoose = require('mongoose'); // to specify a structured schema for data
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+	console.log('Connected correctly to server');
+}, (err) => { 
+	console.log(err);
+});
 
 var app = express();
 
--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -1,42 +1,58 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'); // parse the body of the message
+const mongoose = require('mongoose');
+
+const Dishes = require('../models/dishes');
 
 const dishRouter = express.Router();
 
 dishRouter.use(bodyParser.json());
 
 dishRouter.route('/') //no semicolons except last one
-.all((req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next(); //continue to look for additional app.* below which matches this endpoint
-})
 .get((req,res,next) => {
-    // modification to res in app.all is carried into here
-    res.end('Will send all the dishes to you!');
+    Dishes.find({})
+    .then((dish) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish);
+    }, (err) => next(err))
+    .catch((err) => next(err)); // pass to the overall error handling for application to handle
 })
 .post((req,res,next) => {
-    res.end('Will add the dish: ' + req.body.name + 
-    ' with details: ' + req.body.description);
+    Dishes.create(req.body) // from body parser
+    .then((dish) => {
+        console.log('Dish created ', dish);
+        //console.log('Dish created ', JSON.stringify(dish, null, " "));
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
 .put((req,res,next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /dishes');
 })
 .delete((req,res,next) => {
-    res.end('Deleting all the dishes!');
+    Dishes.remove({})
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);    
+    }, (err) => next(err))
+    .catch((err) => next(err));
 });
 
 
 dishRouter.route('/:dishId') //no semicolons except last one
-.all((req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next(); //continue to look for additional app.* below which matches this endpoint
-})
 .get((req,res,next) => {
-    res.end('Will send details of the dish: ' 
-        + req.params.dishId + ' to you!');
+    Dishes.findById(req.params.dishId)
+    .then((dish) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
 .post((req,res,next) => {
     res.statusCode = 403;
@@ -44,12 +60,24 @@ dishRouter.route('/:dishId') //no semicolons except last one
         + req.params.dishId);
 })
 .put((req,res,next) => {
-    res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: ' + req.body.name + 
-        ' with details: ' + req.body.description);
+    Dishes.findByIdAndUpdate(req.params.dishId, {
+        $set: req.body
+    }, { new: true })
+    .then((dish) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
 .delete((req,res,next) => {
-    res.end('Deleting dish: ' + req.params.dishId);
+    Dishes.findByIdAndRemove(req.params.dishId)
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);    
+    }, (err) => next(err))
+    .catch((err) => next(err));
 });
 
 module.exports = dishRouter;
